15db7ea3cf12e45c82c7d53dfe13aaee36c5e823
mrmazakblu <mrmazakblu@gmail.com>
2016-11-16 19:48:19 -0500

    Update one-click.sh

--- one-click.sh
+++ one-click.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
-#Set our Window Title
-@title ROOT SHELL TEST
-mode 100,30
-echo.--------------------------------------------------------------------------------------------
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 echo [*] BEFORE WE BEGIN THE SCRIPT WILL RUN "ADB DEVICES" AND SEE IF YOU HAVE DRIVERS INSTLLED
 echo [*] THE NEEDED RESPONSE IS SIMILAR TO BELOW 
 echo [*]
 echo [*] List of devices attached
-echo [*] ****************        device
+echo "[*] ****************        device"
 echo [*] 
 echo [*] INSTEAD OF STARS IT WILL BE YOUR SERIAL NUMBER 
 echo [*] IF NO DEVICE LISTED YOU ARE NOT READY TO RUN THIS SCRIPT. CLOSE THIS WINDOW NOW IF NOT READY
 echo [*] 
 echo [*] IF DEVICE IS LISTED PRESS ANY KEY ON COMPUTER TO START
 echo [*]
+echo -n "to continue press [enter]: "
+read start
 adb devices
-read -rsp $'Press enter to continue...\n'
 adb wait-for-device
-clear
+#clear
 echo [*] copying dirtycow to /data/local/tmp/dirtycow
 adb push pushed/dirtycow /data/local/tmp/dirtycow
 sleep 2 
@@ -36,28 +34,30 @@ echo [*] changing permissions on copied files
 adb shell chmod 0777 /data/local/tmp/*
 sleep 2 
 clear
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 echo [*] DONE PUSHING FILES TO PHONE. NOW WE ARE GOING TO TEMP WRITE OVER THE APP_PROCESS
 echo [*] WITH A MODIFIED VERSION THAT HAS lsh IN IT USING A SYSTEM-SERVER AS ROOT SHELL
 echo [*] THIS STEP WILL CAUSE PHONE TO DO A SOFT REBOOT AND WILL NOT RESPOND TO BUTTON PUSHES
 echo [*] 
 adb shell /data/local/tmp/dirtycow /system/bin/app_process32 /data/local/tmp/recowvery-app_process32
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 clear
-echo.--------------------------------------------------------------------------------------------
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 echo [*]WAITING 60 SECONDS FOR ROOT SHELL TO SPAWN
 sleep 60 > nul
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 echo [*] OPENING A ROOT SHELL ON THE NEWLY CREATED SYSTEM_SERVER
 echo [*] CREATE A NEW DIRECTORY AS A TEST
 echo [*] CHANGING PERMISSIONS ON NEW DIRECTORY
 adb shell "/data/local/tmp/busybox nc localhost 11112 < /data/local/tmp/cp_comands.txt"
-read -rsp $'Press enter to continue...\n'
+echo -n "to continue press [enter]: "
+read start
 clear
 echo [*]  
-echo.--------------------------------------------------------------------------------------------
-echo.--------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
+echo --------------------------------------------------------------------------------------------
 echo [*] PRESS ENTER KEY TO FINISH THIS SCRIPT.
-read -rsp $'Press enter to continue...\n'
+echo -n "to continue press [enter]: "
+read start
 exit
